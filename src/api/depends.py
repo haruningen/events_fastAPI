9719c73a0721f07e1223a542858c3aba6e5269b9
Tatiana Kiriushina <tkiriushina@tdera.ru>
2023-07-19 16:02:34 +0300

    fix after mr

--- src/api/depends.py
+++ src/api/depends.py
@@ -1,13 +1,10 @@
-from email.header import Header
-
-import sqlalchemy as sa
 from fastapi import HTTPException, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from common.auth import oauth2_scheme
 from connections.postgresql import async_session
 from models import User
-from utils.users import get_user_by_email, get_user_by_id, token_decode
+from utils.users import token_decode
 
 
 async def get_db() -> AsyncSession:
@@ -20,7 +17,7 @@ async def get_db() -> AsyncSession:
 
 async def get_authed_user(token: str = Depends(oauth2_scheme)) -> User:
     token_data = token_decode(token)
-    user = await get_user_by_id(token_data.user_id)
+    user = await User.get(token_data.user_id)
     # Check if the user exist
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
